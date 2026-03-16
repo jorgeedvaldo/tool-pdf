b5c5a779b06b06ef623e7fb29e37b7aceaa92c1b
jorgeedvaldo <edvaldojorge1996@gmail.com>
2026-03-16 08:07:25 +0100

    feat: create Post model with automatic slug generation and fillable attributes.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -18,9 +19,8 @@ class Post extends Model
         parent::boot();
 
         static::creating(function ($post) {
-            if (empty($post->slug)) {
-                $post->slug = \Illuminate\Support\Str::slug($post->title);
-            }
+            $post->slug = $post->generateSlug($post->title, $post->id);
+            $post->save();
         });
 
         static::updating(function ($post) {
@@ -28,5 +28,14 @@ class Post extends Model
                 $post->slug = \Illuminate\Support\Str::slug($post->title);
             }
         });
+    }
+
+    private function generateSlug($title, $id)
+    {
+        if (static::whereSlug($slug = Str::slug($title))->exists()) {
+            $max = static::whereTitle($title)->latest('id');
+            $slug = $slug . '-' . $id;
+        }
+        return $slug;
     }
 }
